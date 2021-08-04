102df5bcfc7d83c2dbc6e43c08aa16a27a0ebe21
Connor Cowan <ccowan003@comcast.net>
2021-08-04 15:49:37 -0700

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-// const bsTitle = document.getElementById("bs-title");
-// const bsImage = document.getElementById("bs-image");
-// const bsAuthor = document.getElementById("bs-author");
-// const bsLikes = document.getElementById("bs-likes");
-// const bsLikeButton = document.getElementById("bs-like-button");
-// const bsComments = document.getElementById("bs-comments");
-// const bsCommentForm = document.getElementById("bs-comment-form");
 const bookCreateForm = document.getElementById("book-form");
-const bookContainer = document.getElementById("poke-container");
+const bookContainer = document.getElementById("book-container");        ///omfg!!!!!
 
-
-fetch('http://localhost:3000/books')
-    .then((res) => res.json())
-    .then((bookData) => bookData.forEach((book) => renderBooks(book)))
-    //.then((bookData) => renderBooks(bookData));
-    //.then((bookData) => console.log(bookData))
+    fetch('http://localhost:3000/books')
+        .then((res) => res.json())
+        //.then((bookData) => bookData.forEach((book) => renderBooks(book)));
+        //.then((bookData) => renderBooks(bookData));
+        //.then((bookData) => console.log(bookData));
+        //.then(bookData => {bookData.forEach(renderBooks);});
+        //.then(bookData =>{})
 
  function renderBooks(book) {
     
@@ -26,39 +20,45 @@ fetch('http://localhost:3000/books')
     //id, title, author, genre, image & price
     const bookTitle = document.createElement("h2");
     bookTitle.innerText = book.title;
-    //console.log(book.title)
+
+    const bookAuthor = document.createElement("h3");
+    bookAuthor.innerText = book.author;
  
+    const bookGenre = document.createElement("h4")
+    bookGenre.innerText = book.genre
+
     const bookCover = document.createElement("img");
     bookCover.src = book.image;
     bookCover.alt = `${book.title} image`;
     
-    //console.log(book.alt)
-
-    const bookLikes = document.createElement("h3");
+    const bookLikes = document.createElement("h4");
     bookLikes.innerText = "Likes: ";
-
-    const likesNumber = document.createElement("h5");
-    likesNumber.className = "like-num";
-    likesNumber.innerText = book.likes;
 
     const likeButton = document.createElement("button");
     likeButton.className = "like-button";
     likeButton.innerText = "♥";
-    //likeButton.addEventListener("click", increaseLike);
+    likeButton.addEventListener("click", increaseLike);
 
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete-button";
     deleteButton.innerText = "Delete";
-
-    console.log(bookCard)
-
-    bookCard.append(bookTitle, bookCover, bookLikes, likesNumber, likeButton, deleteButton);
+    //bookCard.append(bookTitle, bookAuthor, bookGenre, bookCover)
+    bookCard.append(bookTitle, bookAuthor, bookGenre, bookCover, bookLikes, likeButton, deleteButton);
     bookContainer.appendChild(bookCard);
 }
 
-function init() {
-    getBooks()
-}
+// function getBooks() {
+//     fetch('http://localhost:3000/books')
+//         .then((res) => res.json())
+//         .then((bookData) => bookData.forEach((book) => renderBooks(book)));
+//         //.then((bookData) => renderBooks(bookData));
+//         //.then((bookData) => console.log(bookData));
+//         //.then(bookData => {bookData.forEach(renderBooks);});
+// }
 
-init()
+// function init() {
+//     getBooks();
+// }
+
+// init();
 
