5ab7cd55a8be6b4056e07b8ab1bcd18c9bdfbaf9
Connor Cowan <ccowan003@comcast.net>
2021-08-04 21:30:08 -0700

    Added Likes, Image resize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const bookCreateForm = document.getElementById("book-form");
-const bookContainer = document.getElementById("book-container");        ///omfg!!!!!
-
-    fetch('http://localhost:3000/books')
-        .then((res) => res.json())
-        //.then((bookData) => bookData.forEach((book) => renderBooks(book)));
-        //.then((bookData) => renderBooks(bookData));
-        //.then((bookData) => console.log(bookData));
-        //.then(bookData => {bookData.forEach(renderBooks);});
-        //.then(bookData =>{})
+const bookForm = document.getElementById("book-form");
+const bookContainer = document.getElementById("book-container");
 
  function renderBooks(book) {
     
@@ -18,47 +10,97 @@ const bookContainer = document.getElementById("book-container");        ///omfg!
 
     //Creating HTML elements an assigning variables to our API Properties
     //id, title, author, genre, image & price
-    const bookTitle = document.createElement("h2");
+    const bookTitle = document.createElement("h4");
     bookTitle.innerText = book.title;
 
-    const bookAuthor = document.createElement("h3");
+    const bookAuthor = document.createElement("h6");
     bookAuthor.innerText = book.author;
  
-    const bookGenre = document.createElement("h4")
+    const bookGenre = document.createElement("h6");
     bookGenre.innerText = book.genre
+
+    const bookPrice = document.createElement("h6");
+    bookPrice.innerText = `$${book.price}`;
 
     const bookCover = document.createElement("img");
     bookCover.src = book.image;
     bookCover.alt = `${book.title} image`;
     
-    const bookLikes = document.createElement("h4");
-    bookLikes.innerText = "Likes: ";
-
+    //Instantiating likes, delete for user events
     const likeButton = document.createElement("button");
     likeButton.className = "like-button";
     likeButton.innerText = "♥";
     likeButton.addEventListener("click", increaseLike);
+    
+    const bookLikes = document.createElement("h5");
+    bookLikes.innerText = "Likes: ";
+
+    const likesNumber = document.createElement("h5");
+    likesNumber.className = "like-num";
+    likesNumber.textContent = book.likes;
 
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete-button";
     deleteButton.innerText = "Delete";
-    //bookCard.append(bookTitle, bookAuthor, bookGenre, bookCover)
-    bookCard.append(bookTitle, bookAuthor, bookGenre, bookCover, bookLikes, likeButton, deleteButton);
+    
+    bookCard.append(bookCover, bookTitle, bookAuthor, bookGenre, bookPrice, bookLikes, likesNumber, likeButton, deleteButton);
     bookContainer.appendChild(bookCard);
 }
 
-// function getBooks() {
-//     fetch('http://localhost:3000/books')
-//         .then((res) => res.json())
-//         .then((bookData) => bookData.forEach((book) => renderBooks(book)));
-//         //.then((bookData) => renderBooks(bookData));
-//         //.then((bookData) => console.log(bookData));
-//         //.then(bookData => {bookData.forEach(renderBooks);});
-// }
+function increaseLike(event) {
+    const likesElement = event.target.previousElementSibling;
+    console.log(likesElement)
+    likesElement.innerText = parseInt(likesElement.innerText) + 1;
+}
 
-// function init() {
-//     getBooks();
-// }
+function addBook(event) {
+    event.preventDefault();
+    const bookForm = event.target;
+    const bookTitle = bookForm.querySelector("#title-input").value;
+    const bookAuthor = bookForm.querySelector("#author-input").value;
+    const bookGenre = bookForm.querySelector("#genre-input").value;
+    const bookPrice = bookForm.querySelector("#price-input").value;
+    const bookImage = bookForm.querySelector("#image-input").value;
 
-// init();
+    if (bookTitle !== "" && bookAuthor !== "" && bookGenre !=="" && bookPrice !== "" 
+            && bookImage !== "") {
+        const book = {
+            title: bookTitle,
+            author: bookAuthor,
+            genre: bookGenre,
+            price: bookPrice,
+            image: bookImage,
+            //likes: 0,
+        };
+
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        }
+
+        fetch('http://localhost:3000/pokemons', configObj)
+            .then(resp => resp.json())
+            // .then(pokemon => renderPoke(pokemon))
+            .then(renderBooks)
+        bookForm.reset(); //clearing the form
+    } else {
+        alert("Form Empty");
+    }
+}
+function getBooks() {
+    fetch('http://localhost:3000/books')
+        .then((res) => res.json())
+        .then((bookData) => bookData.forEach((book) => renderBooks(book)));
+        //.then((bookData) => renderBooks(bookData));
+}
+
+function init() {
+    getBooks();
+    bookForm.addEventListener("submit", addBook);
+}
+
+init();
 
